61350abe6dd21c5cceb67bfddf7f75e55b0a7423
Marcus <marek17.10.2001@gmail.com>
2022-05-22 22:24:33 +0200

    Improvement code in files

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 let btn = document.querySelector("button");
-
-let getRandomCat = () => {
+let githubURL = 'https://github.com/';
+let searchDev = () => {
   const loginName = document.getElementById("userLogin").value;
   const url = `https://api.github.com/users/${loginName}`;
   fetch(url)
@@ -8,59 +8,16 @@ let getRandomCat = () => {
     .then((data) => {
 
         document.querySelector('.imageBox').innerHTML = `<img src="${data.avatar_url}">`;
-        document.querySelector('#userName').innerHTML = `${data.name} <br> ${data.login} <br> <br> ${data.bio}`;
+        document.querySelector('.bioInformationBox').innerHTML = 
+        `<p class="userName">${data.name}</p> 
+        <span class="userLogin">${data.login}</span> 
+        <p class="bioInformation"> ${data.bio}</p>
+        <p>
+            <img src="img/GitHub-Mark-32px.png"><a target="_blank" href="${githubURL + data.login}">Github profile</a>
+        </p>`;
         document.querySelector('.repoInformation').innerHTML = `${data.public_repos}&nbsp;${data.followers}&nbsp;${data.following}`;
         document.querySelector('.aboutDevBox').innerHTML = `${data.email}&nbsp;${data.location}`;
         document.querySelector('.socialDevBox').innerHTML = `${data.twitter_username}&nbsp;${data.company}&nbsp;${data.blog}`;
-     
-    // created.innerHTML = `<div$ class="repos">Stworzony: ${data.created_at}</div>`
     });
 };
-btn.addEventListener("click", getRandomCat);
-
-
-
-/*
-    async function loadIntoTable(url, table) {
-    const tableHead = table.querySelector("thead");
-    const tableBody = table.querySelector("tbody");
-    const response = await fetch(url);
-    const { login, name, public_repos, followers, following, created_at, location, bio, twitter_username, company, blog,  avatar_url } = await response.json();
-    const locationText = "Not Available";
-    
-    
-    tableHead.innerHTML = "<tr></tr>";
-    tableBody.innerHTML = "";
-
-    for(const headerText of login) {
-        const headerElement = document.createElement("th");
-
-        headerElement.textContent = headerText;
-        tableHead.querySelector("tr").appendChild(headerElement);
-    }
-    for(const headerText of name) {
-        const headerElement = document.createElement("th");
-
-        headerElement.textContent = headerText;
-        tableHead.querySelector("tr").appendChild(headerElement);
-    }
-    for(const headerText of created_at) {
-        const headerElement = document.createElement("th");
-
-        headerElement.textContent = headerText;
-        tableHead.querySelector("tr").appendChild(headerElement);
-    }
-    
-    for(const headerText of created_at) {
-        const rowElement = document.createElement("tr");
-
-    for(const cellText of created_at) {
-        const cellElement = document.createElement("td");
-
-        cellElement.textContent = cellText;
-        rowElement.appendChild(cellElement);
-    }
-  }
-
-
-}*/
+btn.addEventListener("click", searchDev);
