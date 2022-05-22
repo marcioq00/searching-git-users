f6f6edd4068e7b0ff8fa9da7c09e10fd352dfe06
Marcus <marek17.10.2001@gmail.com>
2022-05-23 00:24:25 +0200

    Added flexbox to div repoInformation

--- js/global.js
+++ js/global.js
@@ -13,9 +13,20 @@ let searchDev = () => {
         <span class="userLogin">${data.login}</span> 
         <p class="bioInformation"> ${data.bio}</p>
         <p>
-            <img src="img/GitHub-Mark-32px.png"><a target="_blank" href="${githubURL + data.login}">Github profile</a>
+            <a target="_blank" class="reference" href="${githubURL + data.login}"><img src="img/GitHub-Mark-32px.png" class="githubAccount">Github profile</a>
         </p>`;
-        document.querySelector('.repoInformation').innerHTML = `${data.public_repos}&nbsp;${data.followers}&nbsp;${data.following}`;
+
+        document.querySelector('.repoInformation').innerHTML = 
+        `
+        <div class="repos">
+            <p>
+                Repository <br>${data.public_repos}
+            </p>
+        </div>
+        <div class="repos">Followers <br>${data.followers}</div>
+        <div class="repos">Following <br>${data.following}</div>
+        `;
+
         document.querySelector('.aboutDevBox').innerHTML = `${data.email}&nbsp;${data.location}`;
         document.querySelector('.socialDevBox').innerHTML = `${data.twitter_username}&nbsp;${data.company}&nbsp;${data.blog}`;
     });
