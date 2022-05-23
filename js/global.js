c74a2da75140e41bd30ab443628c4ca876b1d21e
Marcus <marek17.10.2001@gmail.com>
2022-05-23 14:57:27 +0200

    Removed unecessery code

--- js/global.js
+++ js/global.js
@@ -14,21 +14,36 @@ let searchDev = () => {
         <p class="bioInformation"> ${data.bio}</p>
         <p>
             <a target="_blank" class="reference" href="${githubURL + data.login}"><img src="img/GitHub-Mark-32px.png" class="githubAccount">Github profile</a>
-        </p>`;
+        </p>
+        <img src="img/icons8-location-48.png" class="githubAccount" alt="location logo">${data.location}`;
 
         document.querySelector('.repoInformation').innerHTML = 
         `
         <div class="repos">
             <p>
-                Repository <br>${data.public_repos}
+                <span class="informationCount">Repository</span> <br>
+                <span class="repositoryCount">${data.public_repos}</span>
             </p>
         </div>
-        <div class="repos">Followers <br>${data.followers}</div>
-        <div class="repos">Following <br>${data.following}</div>
+        <div class="repos">
+            <p>
+                <span class="informationCount">Followers</span> <br>
+                <span class="repositoryCount">${data.followers}</span>
+            </p>
+        </div>
+        <div class="repos">
+            <p>
+                <span class="informationCount">Following </span> <br>
+                <span class="repositoryCount">${data.following}</span>
+            </p>
         `;
-
-        document.querySelector('.aboutDevBox').innerHTML = `${data.email}&nbsp;${data.location}`;
-        document.querySelector('.socialDevBox').innerHTML = `${data.twitter_username}&nbsp;${data.company}&nbsp;${data.blog}`;
+        document.querySelector('.socialDevBox').innerHTML = 
+        `<img src="img/icons8-twitter-48.png" alt="twitter logo">${data.twitter_username}
+        <img src="img/icons8-company-48.png" alt="comapny logo">${data.company}
+        <img src="img/icons8-website-48.png" alt="website logo">${data.blog}`;
     });
 };
 btn.addEventListener("click", searchDev);
+
+
+/* <a target="_blank" href="https://icons8.com/icon/1349/website">Website</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */
