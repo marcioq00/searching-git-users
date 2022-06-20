4a3db7c4d4b9ea6636d2d47cda5513f44bc8616c
Marcus <marek17.10.2001@gmail.com>
2022-06-20 17:22:39 +0200

    change border radius better view for repo information and centering text

--- js/global.js
+++ js/global.js
@@ -3,10 +3,10 @@ let githubURL = "https://github.com/";
 let twitterURL = "https://twitter.com/";
 
 let createPopupError = () => {
-    const popup = document.getElementById("myPopup");
-    popup.classList.remove("hidden");
-    popup.classList.toggle("show");
-}
+  const popup = document.getElementById("myPopup");
+  popup.classList.remove("hidden");
+  popup.classList.toggle("show");
+};
 
 let checkInput = (field) => {
   const reg = new RegExp(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i);
@@ -45,7 +45,7 @@ let searchDev = () => {
     .then((data) => {
       document.querySelector(
         ".imageBox"
-      ).innerHTML = `<img src="${data.avatar_url}">`;
+      ).innerHTML = `<img src="${data.avatar_url}" alt="Avatar ${data.name}">`;
       document.querySelector(
         ".bioInformationBox"
       ).innerHTML = `<p class="userName">${data.name}</p> 
@@ -57,52 +57,24 @@ let searchDev = () => {
             }"><img src="img/GitHub-Mark-32px.png" class="githubAccount">Github profile</a>
         </p>
         `;
-      document.querySelector(".repoInformation").innerHTML = `
-        <div class="repos">
-            <p>
-                <span class="informationCount">Repository</span> <br>
-                <span class="repositoryCount">${data.public_repos}</span>
-            </p>
-        </div>
-        <div class="repos">
-            <p>
-                <span class="informationCount">Followers</span> <br>
-                <span class="repositoryCount">${data.followers}</span>
-            </p>
-        </div>
-        <div class="repos">
-            <p>
-                <span class="informationCount">Following </span> <br>
-                <span class="repositoryCount">${data.following}</span>
-            </p>
-        `;
+      document.querySelector("#repos").innerHTML = `${data.public_repos}`;
+      document.querySelector("#followers").innerHTML = ` ${data.followers}`;
+      document.querySelector("#following").innerHTML = `${data.following}`;
 
-      document.querySelector(
-        ".socialDevBox"
-      ).innerHTML = `<div class="socialDevBoxParagraph">
-            <img src="img/icons8-company-48.png" alt="comapny logo" class="socialDevBoxIcon">${
-              data.company
-            }
-        </div>
-        <div class="socialDevBoxParagraph2">
-            <a href="${data.blog}" class="referenceSite" target="_blank">
-                <img src="img/icons8-website-48.png" alt="website logo" class="socialDevBoxIcon">${
-                  data.blog
-                }
-            </a>
-        </div>
-        <div class="twitter">
-            <a href="${
-              twitterURL + data.twitter_username
-            }" target="_blank" class="referenceSite">
-                <img src="img/icons8-twitter-48.png" alt="twitter logo" class="socialDevBoxIcon">Twitter
-            </a>
-        </div>
-        <div class="location">
-            <img src="img/icons8-location-48.png" class="socialDevBoxIcon" alt="location logo">${
-              data.location
-            }
-        </div>`;
+      document.querySelector(".companyName").innerHTML = `${data.company}`;
+      let websiteURL = `${data.blog}`;
+
+      function changeHref() {
+        document.getElementById("strona").setAttribute("href", websiteURL);
+        document.querySelector(".web").innerHTML = websiteURL;
+      }
+      changeHref();
+      let twitts = `${data.twitter_username}`;
+      document
+        .getElementById("twitts")
+        .setAttribute("href", twitterURL + twitts);
+
+      document.querySelector(".location-text").innerHTML = `${data.location}`;
     });
 };
 
